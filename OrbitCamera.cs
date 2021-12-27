41df71385273f8ac26da7e3615ac8246f142847f
LoganTheOverlord <42440842+LoganTheOverlord@users.noreply.github.com>
2021-12-27 21:25:30 +0100

    typo fix.

--- OrbitCamera.cs
+++ OrbitCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WolfController : MonoBehaviour
+public class OrbitCamera : MonoBehaviour
 {
     Camera main;
     public float rot;
